c3d34ded0a8c5233e8e2b998f85718de43c31d91
oh wontaek <oh.wt@astkorea.net>
2025-11-11 16:53:40 +0900

    타입스크립트 체크

--- src/utils/api/config.ts
+++ src/utils/api/config.ts
@@ -45,7 +45,8 @@ export const API_PATHS = {
   SIMULATION_CHECK: getApiPath('api/v1/simulation/check'),
   SIMULATION_QUICK_LIST: getApiPath('api/v1/simulation_auto/list'),
   SIMULATION_DETAIL: (uuid: string) => getApiPath(`api/v1/simulation/${uuid}`),
-  SIMULATION_DETAIL_SUMMARY: () => getApiPath(`api/v1/simulation/00000000-0000-0000-0000-000000000001/vulnerable-facilities`),
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  SIMULATION_DETAIL_SUMMARY: (_uuid: string) => getApiPath(`api/v1/simulation/00000000-0000-0000-0000-000000000001/vulnerable-facilities`),
   //SIMULATION_DETAIL_SUMMARY: (uuid: string) => getApiPath(`api/v1/simulation/${uuid}/vulnerable-facilities`),
   SIMULATION_DELETE: getApiPath(`/api/v1/simulation`),
   SIMULATION_UPDATE_PRIVACY: (uuid: string) => getApiPath(`/api/v1/simulation/${uuid}/privacy`),
